8a25b9671349d8c723935100f685d5f56bda0986
Shiv10 <shivanshsharma2012@gmail.com>
2024-05-26 03:08:01 +0530

    feat: done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="photo-frame">
+          <div className="photo"><img src="https://random.imagecdn.app/500/200" alt="Photo 1" /></div>
+          <div className="photo"><img src="https://random.imagecdn.app/500/210" alt="Photo 2" /></div>
+          <div className="photo"><img src="https://random.imagecdn.app/500/220" alt="Photo 3" /></div>
+          <div className="photo"><img src="https://random.imagecdn.app/500/150" alt="Photo 4" /></div>
+        </div>
+        <p>Made with 💖 for Aarushi   <font size="2">™ © Shivansh</font></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
